24d3b2e878b8fb253430113005c3a15d351b7a2c
JOSHIK <joshikroshan4021@gmail.com>
2024-11-03 10:35:08 +0000

    footer designed

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,10 +1,19 @@
 import { Separator } from "@/components/ui/separator";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa6";
 
 export default function Footer() {
   return (
-    <footer>
-      <Separator className="w-full my-4" />
-      <div className="text-center text-sm text-muted-foreground pb-8">
+    <footer className="mt-20 sm:px-60">
+      <Separator className="w-full mt-4 mb-6" />
+      <span className="text-sm text-muted-foreground pb-8">Follow Us</span>
+      <div className="flex items-center gap-4 mt-2 mb-4">
+        <FaXTwitter className="text-2xl" />
+        <FaLinkedin className="text-2xl" />
+        <FaGithub className="text-2xl" />
+      </div>
+      <div className="text-sm text-muted-foreground pb-8 mt-20">
         © {new Date().getFullYear()} Automation. All rights reserved.
       </div>
     </footer>
